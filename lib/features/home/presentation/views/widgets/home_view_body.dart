7ad4b98223b046f03295c1c9934429eb16fa4270
abdoo <abdoalgamal84@gmail.com>
2024-03-23 10:56:30 +0200

    add best seller list view part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/utils/assets_data.dart';
 import 'custom_app_bar.dart';
 import 'featured_books_list_view.dart';
 
@@ -25,6 +26,32 @@ class HomeViewBody extends StatelessWidget {
                 style: Styles.titleMedium,
               ),
             ],
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.6 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(7),
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(AssetsData.kListImageItem))),
+            ),
           )
         ],
       ),
