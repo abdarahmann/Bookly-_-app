c4c18e638caf71998281fb95a89e89c99811cc86
abdoo <abdoalgamal84@gmail.com>
2024-04-17 02:29:30 +0200

    refactor search feature

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../../../../core/utils/text_styles.dart';
-import '../../../../home/presentation/views/widgets/best_seller_list_view_item.dart';
+import 'custom_search_text_field.dart';
+import 'search_result_listview.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
@@ -11,7 +11,7 @@ class SearchViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.all(8.0),
+      padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -33,48 +33,5 @@ class SearchViewBody extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class CustomSearchTextField extends StatelessWidget {
-  const CustomSearchTextField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          hintText: "Search",
-          suffixIcon: const Opacity(
-            opacity: 0.5,
-            child: Icon(
-              FontAwesomeIcons.magnifyingGlass,
-              size: 28,
-            ),
-          ),
-          enabledBorder: inputBorder(),
-          focusedBorder: inputBorder()),
-    );
-  }
-
-  OutlineInputBorder inputBorder() {
-    return OutlineInputBorder(
-        borderSide: const BorderSide(color: Colors.white),
-        borderRadius: BorderRadius.circular(15));
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.only(bottom: 20),
-            child: BestSellerListViewItem(),
-          );
-        });
   }
 }
