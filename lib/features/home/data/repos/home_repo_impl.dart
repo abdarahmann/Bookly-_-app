2fafcb06746df63a0a0909ed84e461fdefd9c09f
abdoo <abdoalgamal84@gmail.com>
2024-04-20 13:19:49 +0200

    done handling failure

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -10,9 +10,25 @@ class HomeRepoImpl extends HomeRepo {
   HomeRepoImpl(this.apiService);
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetcFeturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetcFeturedBooks()async {
+     try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&q=subject:programming');
+      List<BookModel> books = [];
+      for (var item in data["items"]) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(errMessage: e.toString()));
+    }
   }
+  
+
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
